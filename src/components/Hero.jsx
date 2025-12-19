3c18189b2b0bbe4b6f9b7e8c5f320d5588495b1f
Rahul Tripathi <tripathirahul796@gmail.com>
2025-12-19 20:19:27 +0530

    updated name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
           >
             Hi, I'm{' '}
             <span className="bg-gradient-to-r from-primary-400 via-primary-500 to-primary-600 bg-clip-text text-transparent">
-              Your Name
+              Rahul Tripathi
             </span>
           </motion.h1>
 
