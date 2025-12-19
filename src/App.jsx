ea459a2f36865d37be757c723e40d605efef1a03
tripathirahul796 <46559539+tripathirahul796@users.noreply.github.com>
2025-12-19 13:06:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
